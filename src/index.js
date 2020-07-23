0340d5ec5cd4874fd1e45796c83e31a46ac26625
Cheon YeongSeok <dudtjr913@naver.com>
2020-07-23 20:17:00 +0900

    second solo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./main/App";
+import Rou from "./router/rou";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Rou />, document.getElementById("root"));
